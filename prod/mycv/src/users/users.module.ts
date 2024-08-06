d4d8c6c2ece97975489d7176ed5e59dc66eaf47d
sreedevi-ui <sarosree27@gmail.com>
2024-08-05 21:33:37 -0700

    Learning NestJS

---
+++ prod/mycv/src/users/users.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import{APP_INTERCEPTOR} from'@nestjs/core';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import{User}from'./user.entity';
+import{CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+@Module({
+  imports:[TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService,AuthService,
+    {
+      provide:APP_INTERCEPTOR,
+      useClass :CurrentUserInterceptor
+}],
+})
+export class UsersModule {}
---
+++ prod/mycv/test/auth.e2e-spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup requests', () => {
+    const email='asdlkj09@akl.com';
+    return request(app.getHttpServer())
+    .post('/auth/signup')
+    .send({email,password:'asdl'})
+    .expect(201)
+    .then((res) =>{
+        const{ id,email}=res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email)
+
+    });
+
+    
+      
+
+
+  });
+});
